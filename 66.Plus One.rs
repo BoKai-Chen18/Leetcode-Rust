bc937bebf8679951508410fc03f2c0d76b5d0aad
BoKai-Chen18 <aa0975704203@gmail.com>
2025-06-05 15:21:44 +0800

    Update 66.Plus One.rs

--- 66.Plus One.rs
+++ 66.Plus One.rs
@@ -1,1 +1,20 @@
+impl Solution {
+    pub fn plus_one(digits: Vec<i32>) -> Vec<i32> {
+        let n = digits.len();
+        let mut ans = Vec::new();
+        let mut carry = 1;
 
+        for i in (0..n).rev() {
+            let sum = digits[i] + carry;
+            ans.push(sum % 10);
+            carry = sum / 10;
+        }
+
+        if carry == 1 {
+            ans.push(1);
+        }
+
+        ans.reverse();
+        ans
+    }
+}
